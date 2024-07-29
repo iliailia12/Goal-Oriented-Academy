a97791464b1152d47e1f38f6cc9bbb6938f2d1eb
iliailia12 <iliakviciani392@gmail.com>
2024-07-29 12:08:21 +0100

    commit

--- Day 18/Classwork/task5.py
+++ Day 18/Classwork/task5.py
@@ -3,3 +3,15 @@
    ორი ელემენტი საბოლოოდ კი დაბეჭდეთ ჩამოჭრილი სიები. ამის შემდეგ მინუსებიანი
  ინდექსებით გამოიტანეთ მარჯვნიდან
  მესამე და მეოთხე ელემენტები'''
+
+
+car_names = ["Tesla Model S", "BMW M3", "Audi A4", "Porsche 911", "Mercedes-Benz S-Class"]
+first_three = car_names[:3]
+last_two = car_names[-2:]
+print("First 3 elements:", first_three)
+print("Last 2 elements:", last_two)
+third_from_right = car_names[-3]
+fourth_from_right = car_names[-4]
+print("Third element from the right:", third_from_right)
+print("Fourth element from the right:", fourth_from_right)
+
