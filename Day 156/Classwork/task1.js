2d96cff2eb3e363e01376de3075b4e7433aeeef1
iliailia12 <iliakviciani392@gmail.com>
2025-06-22 13:08:46 -0700

    second commit

---
+++ Day 156/Classwork/task1.js
@@ -0,0 +1,6 @@
+// Random User Generator
+// API: https://randomuser.me/api/
+// 🎯 Goal: On button click, fetch and display a random person’s name, picture, and email.
+// 💡 Bonus: Add a "Next User" button to load a new one.
+// დრო: 15წ
+
